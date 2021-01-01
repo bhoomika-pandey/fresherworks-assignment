9dad53b9aca3ef4382ac9e1d5519cfc9b71bdaa0
bhoomika-pandey <erbhoo@gmail.com>
2021-01-01 20:48:15 +0530

    tested using some key_name

--- execute.py
+++ execute.py
@@ -4,41 +4,41 @@ from main_code import*
 #importing the main file
 
 
-x.create("sastra",25)
-#creates a key with key_name,value given and with no time-to-live property
+x.create("fresher",25)
+#creates a key with key_name,value given and with no ttl (time-to-live) property
 
 
 x.create("src",70,3600) 
-#creates a key with key_name,value given and with time-to-live property value given(number of seconds)
+#creates a key with key_name,value given and with ttl property value given(number of seconds)
 
 
-x.read("sastra")
+x.read("fresher")
 #returns the value of the respective key in Jasonobject format 'key_name:value'
 
 
 x.read("src")
-#returns the value of the respective key in Jasonobject format if the TIME-TO-LIVE IS NOT EXPIRED else it returns an ERROR
+#returns the value of the respective key in Jasonobject format if the ttl is not expired else it returns an ERROR
 
 
-x.create("sastra",50)
+x.create("fresher",50)
 #it returns an ERROR since the key_name already exists in the database
-#To overcome this error 
+#to overcome this error 
 #either use modify operation to change the value of a key
 #or use delete operation and recreate it
 
 
-x.modify("sastra",55)
+x.modify("fresher",55)
 #it replaces the initial value of the respective key with new value 
 
  
-x.delete("sastra")
+x.delete("fresher")
 #it deletes the respective key and its value from the database(memory is also freed)
 
 #we can access these using multiple threads like
-t1=Thread(target=(create or read or delete),args=(key_name,value,timeout)) #as per the operation
+t1=Thread(target=(create or read or delete),args=(key_name,value,timeout))
 t1.start()
 t1.sleep()
-t2=Thread(target=(create or read or delete),args=(key_name,value,timeout)) #as per the operation
+t2=Thread(target=(create or read or delete),args=(key_name,value,timeout))
 t2.start()
 t2.sleep()
 
